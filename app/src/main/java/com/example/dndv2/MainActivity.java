bc33550cfd857ff47358e912d4570fdbda4720ec
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-04-14 16:12:11 -0700

    misc

--- app/src/main/java/com/example/dndv2/MainActivity.java
+++ app/src/main/java/com/example/dndv2/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * link to kaban board: https://docs.google.com/spreadsheets/d/1bDCZd-A317ZPMwXX4LJmkg1AMy7mqXyuz8Pd6ddEv6Y/edit#gid=0
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
