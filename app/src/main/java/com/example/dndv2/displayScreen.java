588d9951285231c4fad4a1b09fafb140cc3f4bdb
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2021-04-19 12:56:15 -0700

    table display

---
+++ app/src/main/java/com/example/dndv2/displayScreen.java
@@ -0,0 +1,14 @@
+package com.example.dndv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class displayScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_screen);
+    }
+}
