8d0b153cd9f8bac6ea4e2b14bc21013d9d955833
Grace0Hud <gracein3d@gmail.com>
2021-04-30 19:56:24 -0700

    sign up possible

--- app/src/main/java/com/example/dndv2/MainActivity.java
+++ app/src/main/java/com/example/dndv2/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.dndv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.lang.invoke.ConstantCallSite;
 
 /**
  * link to kaban board: https://docs.google.com/spreadsheets/d/1bDCZd-A317ZPMwXX4LJmkg1AMy7mqXyuz8Pd6ddEv6Y/edit#gid=0
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private EditText passET, emailET;//signin
+
+    private EditText signupPassET, signUpREPassET, signupEmailET, signUpNameET;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingbar;
+    Button signin, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,13 +43,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.signupbtn);
-        button.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        loadingbar = new ProgressDialog(this);
+
+        Button signup = (Button) findViewById(R.id.signupbtn);
+        signup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 changeScreen();
             }
         });
     }
+
 
     public void changeScreen()//changes screen after player wins
     {
@@ -38,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
+        signupEmailET = (EditText)promptView.findViewById(R.id.emailsignup);
+        signupPassET = (EditText)promptView.findViewById(R.id.passwordsignup);
+        signUpREPassET = (EditText)promptView.findViewById(R.id.retypePassword);
+        signUpNameET = (EditText)promptView.findViewById(R.id.name);
         // set prompts.xml to be the layout file of the alertdialog builder
         alertDialogBuilder.setView(promptView);
 
@@ -46,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 .setCancelable(false)
                 .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        signUp();
                         dialog.cancel();
                     }
                 })
@@ -60,6 +91,60 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertD = alertDialogBuilder.create();
 
         alertD.show();
+    }
+
+    private void signUp()
+    {
+        String newEmail = signupEmailET.getText().toString();
+        String newPass = signupPassET.getText().toString();
+        String retypePass = signUpREPassET.getText().toString();
+        String newName = signUpNameET.getText().toString();
+
+
+        if(TextUtils.isEmpty(newEmail))
+        {
+            Toast.makeText(this, "must enter an email", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(newPass))
+        {
+            Toast.makeText(this, "must enter password", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(retypePass))
+        {
+            Toast.makeText(this, "must retype password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!newPass.equals(retypePass))
+        {
+            Toast.makeText(this, "password and retype password must match", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(newName))
+        {
+            Toast.makeText(this, "must enter a name", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingbar.setTitle("Creating New Account");
+            loadingbar.setMessage("Please wait while your account is being created");
+            loadingbar.show();
+            loadingbar.setCanceledOnTouchOutside(true);
+
+            mAuth.createUserWithEmailAndPassword(newEmail, newPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this, "account created", Toast.LENGTH_SHORT).show();
+                        loadingbar.dismiss();
+                    }
+                    else
+                    {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(MainActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                        loadingbar.dismiss();
+                    }
+                }
+            });
+        }
     }
 
 }
