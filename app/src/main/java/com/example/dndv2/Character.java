252448e85125d03203fa8b04b433f1a934345e44
Grace0Hud <gracein3d@gmail.com>
2021-05-03 15:51:02 -0700

    character constructors + tests

--- app/src/main/java/com/example/dndv2/Character.java
+++ app/src/main/java/com/example/dndv2/Character.java
@@ -9,17 +9,120 @@ public class Character
     static Dice d6 = new Dice(6);
     private String name;
     private int level, str, dex, con, intel, wis, cha;
+    private int[] stats = {str, dex, con, intel, wis, cha};
 
+    public  Character()
+    {
+        this.name = "Sam Smorkle";
+        this.level = 1;
+        setUp();
+    }
+    public Character(String name)
+    {
+        this.name = name;
+        this.level = 1;
+        setUp();
+    }
     public Character(String name, int level)
     {
         this.name = name;
         this.level = level;
         setUp();
     }
+    public Character(String name, int level, int[] stats)
+    {
+        this.name = name;
+        this.level = level;
+        setStats(stats);
+    }
 
     private void setUp()
     {
-        rollStat();
+        for(int i = 0; i < stats.length; i++)
+        {
+            stats[i] = rollStat();
+        }
+    }
+
+    //getters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getStr() {
+        return str;
+    }
+
+    public int getDex() {
+        return dex;
+    }
+
+    public int getCon() {
+        return con;
+    }
+
+    public int getIntel() {
+        return intel;
+    }
+
+    public int getWis() {
+        return wis;
+    }
+
+    public int getCha() {
+        return cha;
+    }
+
+    public int[] getStats() {
+        return stats;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //setters
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setStr(int str) {
+        this.str = str;
+    }
+
+    public void setDex(int dex) {
+        this.dex = dex;
+    }
+
+    public void setCon(int con) {
+        this.con = con;
+    }
+
+    public void setIntel(int intel) {
+        this.intel = intel;
+    }
+
+    public void setWis(int wis) {
+        this.wis = wis;
+    }
+
+    public void setCha(int cha) {
+        this.cha = cha;
+    }
+
+    public void setStats(int[] stats) {
+        this.str = stats[0];
+        this.dex = stats[1];
+        this.con = stats[2];
+        this.intel = stats[3];
+        this.wis = stats[4];
+        this.cha = stats[5];
     }
 
     public static int rollStat()
@@ -45,5 +148,14 @@ public class Character
         Log.i("final stat:", String.valueOf(stat));
         return stat;
 
+    }
+    public String toString()
+    {
+        String output = "";
+        output += "-----" + name + "----";
+        output += "\nlevel: " + level;
+        output += "\nstr: " + str + "\tdex: " + dex + "\tcon: " + con;
+        output += "\nint: " + intel + "\twis: " + wis + "\tcha: " + cha;
+        return output;
     }
 }
--- app/src/test/java/com/example/dndv2/CharacterTests.java
+++ app/src/test/java/com/example/dndv2/CharacterTests.java
@@ -5,10 +5,55 @@ import static org.junit.Assert.*;
 
 public class CharacterTests
 {
+    int[] stats = {3,12,13,15,6,8};
     @Test
     public void rollStat()
     {
         int stat = Character.rollStat();
-        assertTrue(stat < 20);
+        assertTrue(stat < 20 && stat > 2);
+    }
+    @Test
+    public void defaultConstr()
+    {
+        Character character = new Character();
+        String output = "";
+        output += "-----Sam Smorkle----";
+        output += "\nlevel: " + 1;
+        output += "\nstr: " + character.getStr() + "\tdex: " + character.getDex() + "\tcon: " + character.getCon();
+        output += "\nint: " + character.getIntel() + "\twis: " + character.getWis() + "\tcha: " + character.getCha();
+        assertEquals(output, character.toString());
+    }
+    @Test
+    public void nameConstr()
+    {
+        Character character = new Character("Cedar");
+        String output = "";
+        output += "-----Cedar----";
+        output += "\nlevel: " + 1;
+        output += "\nstr: " + character.getStr() + "\tdex: " + character.getDex() + "\tcon: " + character.getCon();
+        output += "\nint: " + character.getIntel() + "\twis: " + character.getWis() + "\tcha: " + character.getCha();
+        assertEquals(output, character.toString());
+    }
+    @Test
+    public void nameLevelContr()
+    {
+        Character character = new Character("Cedar", 5);
+        String output = "";
+        output += "-----Cedar----";
+        output += "\nlevel: " + 5;
+        output += "\nstr: " + character.getStr() + "\tdex: " + character.getDex() + "\tcon: " + character.getCon();
+        output += "\nint: " + character.getIntel() + "\twis: " + character.getWis() + "\tcha: " + character.getCha();
+        assertEquals(output, character.toString());
+    }
+    @Test
+    public void nameLevelStatsContr()
+    {
+        Character character = new Character("Cedar", 5, stats);
+        String output = "";
+        output += "-----Cedar----";
+        output += "\nlevel: " + 5;
+        output += "\nstr: " + stats[0] + "\tdex: " + stats[1] + "\tcon: " + stats[2];
+        output += "\nint: " + stats[3] + "\twis: " + stats[4] + "\tcha: " + stats[5];
+        assertEquals(output, character.toString());
     }
 }
